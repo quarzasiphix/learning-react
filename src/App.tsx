0314f5231e4e43f6ef42d0ff23975a73032fa030
quarza <quarrysiphix@gmail.com>
2023-02-15 09:24:16 +0000

    nested router

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,41 @@
-import { useState } from 'react'
+import { createBrowserRouter, 
+  Routes, 
+  Route, 
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+import { useState, useEffect } from 'react'
+import BlogList from './pages/BlogList'
+import HomePage from './pages/Home'
+import Contact from './layout/help/contact'
+import Faq from './layout/help/faq'
+ 
+import RootLayout from './layout/RootLayout'
+
+//import Navbar from './Navbar'
+
 import './index.css'
+import HelpLayout from './layout/HelpLayout'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path="blogs" element={<BlogList title='nigga blogs'/>}/>
+        <Route path="help" element={<HelpLayout/>}>
+          <Route path="faq" element={<Faq/>}/>
+          <Route path="contact" element={<Contact/>}/>
+        </Route>
 
+        <Route path="*" element={}/>
+      </Route>
+  )
+)
 const App = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'site', body: 'sup', author: 'me', id: 1},
-    { title: 'niggas', body: 'wazaa sup', author: 'niggame', id: 2},
-    { title: 'test e nigga', body: 'wazaa sup body', author: 'niggers', id: 3 }
-  ])
   return(
-    <div className='home'>
-      { blogs.map((blog) => (
-        <div className="blog-preview">
-          <h2>{blog.title}</h2>
-          <p> author { blog.author}</p>
-        </div>
-      ))}
-    </div>
+    <RouterProvider router={router} />
   )
-  /*
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-  */
 }
 
 export default App
