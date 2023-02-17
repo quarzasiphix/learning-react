b2bb70ab87bba1a4bdc73518cde38e09e53db594
quarza <quarrysiphix@gmail.com>
2023-02-17 11:06:12 +0000

    learning more css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,16 @@ const HomePage = () => {
     const [count, setCount] = useState(0)
     
     return (
-        <div className='home'>
-            <h1> yaaa its home</h1>
-            <div className='fafosec'>
+        <> <div className='fafosec'>
                 <div className='fafo'>
-                <p> {count} </p>
                     <button onClick={() => { setCount(count+ + 1) }}> test </button>
                 </div>
             </div>
+        <div className="home">
+            <h1> yaaa its home</h1>
+                <p> {count} </p>
         </div>
+        </>
     )    
 }
 
